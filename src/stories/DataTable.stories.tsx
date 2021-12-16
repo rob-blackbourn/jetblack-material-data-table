42fca24c7418a9ebdb88e608d6a1ef34d2b0504c
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-12-16 16:07:26 +0000

    Added roboto font to storybook

--- src/stories/DataTable.stories.tsx
+++ src/stories/DataTable.stories.tsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import * as React from 'react'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import SimpleDataTable from './SimpleDataTable'
 import ComplexDataTable from './ComplexDataTable'
 import RowDetailDataTable from './RowDetailDataTable'
