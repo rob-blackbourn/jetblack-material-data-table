c90b01b6df630cc325d5574a5fe3f968e06b7bb7
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-06-08 17:07:16 +0100

    Exported handler types

--- src/components/types.ts
+++ src/components/types.ts
@@ -5,50 +5,60 @@ export interface Row {
   [key: string]: any
 }
 
+export type ColumnGetValueHandler = (
+  row: Row,
+  column: Column,
+  columns: Column[],
+  rows: Row[],
+  context: any
+) => any
+
+export type ColumnFormatValueHandler = (
+  value: any,
+  row: Row,
+  column: Column,
+  columns: Column[],
+  rows: Row[],
+  context: any
+) => string
+
+export type ColumnRenderValueHandler = (
+  value: any,
+  row: Row,
+  column: Column,
+  columns: Column[],
+  rows: Row[],
+  context: any
+) => React.ReactNode | string
+
+export type ColumnSearchHandler = (
+  text: string,
+  row: Row,
+  column: Column,
+  columns: Column[],
+  rows: Row[],
+  context: any
+) => boolean
+
+export type ColumnCompareHandler = (
+  lhs: Row,
+  rhs: Row,
+  column: Column,
+  columns: Column[],
+  rows: Row[],
+  context: any
+) => -1 | 0 | 1
+
 export interface Column {
   id: string
   title?: string
   hide?: boolean
   align?: 'inherit' | 'left' | 'center' | 'right' | 'justify'
-  getValue?: (
-    row: Row,
-    column: Column,
-    columns: Column[],
-    rows: Row[],
-    context: any
-  ) => any
-  formatValue?: (
-    value: any,
-    row: Row,
-    column: Column,
-    columns: Column[],
-    rows: Row[],
-    context: any
-  ) => string
-  renderValue?: (
-    value: any,
-    row: Row,
-    column: Column,
-    columns: Column[],
-    rows: Row[],
-    context: any
-  ) => React.ReactNode | string
-  search?: (
-    text: string,
-    row: Row,
-    column: Column,
-    columns: Column[],
-    rows: Row[],
-    context: any
-  ) => boolean
-  compare?: (
-    lhs: Row,
-    rhs: Row,
-    column: Column,
-    columns: Column[],
-    rows: Row[],
-    context: any
-  ) => -1 | 0 | 1
+  getValue?: ColumnGetValueHandler
+  formatValue?: ColumnFormatValueHandler
+  renderValue?: ColumnRenderValueHandler
+  search?: ColumnSearchHandler
+  compare?: ColumnCompareHandler
   sx?: SxProps<Theme>
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import { Row, Column, ColumnSortMap } from './components/types'
+import {
+  Row,
+  Column,
+  ColumnCompareHandler,
+  ColumnFormatValueHandler,
+  ColumnGetValueHandler,
+  ColumnRenderValueHandler,
+  ColumnSearchHandler,
+  ColumnSortMap,
+} from './components/types'
 
-export type { Row, Column, ColumnSortMap }
+export type {
+  Row,
+  Column,
+  ColumnCompareHandler,
+  ColumnFormatValueHandler,
+  ColumnGetValueHandler,
+  ColumnRenderValueHandler,
+  ColumnSearchHandler,
+  ColumnSortMap,
+}
 
 export { default as DataTable } from './components/DataTable'
